c70ec6747e9a1516ff501b9c542c6bd8d652c131
Mark Lynch <markl@sherpa.net.au>
2022-01-28 18:08:28 +1100

    sleep until puma?

--- lib/test_ruby_gem/puma_exporter.rb
+++ lib/test_ruby_gem/puma_exporter.rb
@@ -1,9 +1,13 @@
 module TestRubyGem
     class PumaExporter < BaseExporter
         def call
-            puts "ping pong"
+            sleep(1) until puma?
             stats = JSON.parse(Puma.stats, symbolize_names: true)
             pp stats
+        end
+
+        def puma?
+            !!defined?(::Puma)
         end
     end
 end
