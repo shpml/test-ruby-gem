7cbbc8628b6ef2abcbf6069e9c4a12601fe869cc
Mark Lynch <markl@sherpa.net.au>
2022-01-28 17:29:07 +1100

    test

--- lib/test_ruby_gem.rb
+++ lib/test_ruby_gem.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
 require_relative "test_ruby_gem/version"
-require_relative "test_ruby_gem/puma_exporter"
+require "test_ruby_gem/puma_exporter"
+require "test_ruby_gem/base_exporter"
 
 module TestRubyGem
   class Error < StandardError; end
