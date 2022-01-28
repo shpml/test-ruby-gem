e49cdce58d0264a815eebe415111f2c3fe9664b7
Mark Lynch <markl@sherpa.net.au>
2022-01-28 16:36:26 +1100

    test gem

---
+++ lib/test_ruby_gem/base_exporter.rb
@@ -0,0 +1,19 @@
+module TestRubyGem
+    class BaseExporter
+        attr_reader :timer_task
+
+        def initialize
+            @timer_task = Concurrent::Ruby::TimerTask.new(execution_interval: 5, timeout_interval: 5) do
+            call
+            end
+        end
+
+        def start
+            timer_task.execute
+        end
+
+        def call
+            raise NotImplementedError
+        end
+    end
+end
---
+++ lib/test_ruby_gem/puma_exporter.rb
@@ -0,0 +1,10 @@
+require 'base_exporter'
+
+module TestRubyGem
+    class PumaExporter < BaseExporter
+        def call
+            stats = Puma.stats
+            pp stats
+        end
+    end
+end
